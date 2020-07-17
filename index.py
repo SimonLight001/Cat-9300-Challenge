cb33c32f28557bf40650d0d85bb4190953285093
Simon Light <sipoe900@gmail.com>
2020-07-17 15:25:18 +0100

    adding flask starter

--- index.py
+++ index.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 print("--- Starting teams bot ---")
 
+data = {'roomId':'room','text':'Tesing123'}
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    r = requests.post('http://webexapis.com/v1/messages', data=data)
+    return r.text
 
 
 if __name__ == "__main__":
