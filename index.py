67607aed430a6585799c349e90b88e5290f536ab
Simon Light <sipoe900@gmail.com>
2020-07-17 09:17:07 +0100

    spinning up flask

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+print("--- Starting teams bot ---")
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("80"), debug=True)
