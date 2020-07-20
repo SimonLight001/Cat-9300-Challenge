f7cad77ea1568110e249b2b3b7cf50ec471999c5
Simon Light <sipoe900@gmail.com>
2020-07-20 15:30:07 +0100

    refactor index.py

--- index.py
+++ index.py
@@ -6,25 +6,24 @@ from webexteamssdk import WebexTeamsAPI, Webhook
 app = Flask(__name__)
 api = WebexTeamsAPI(access_token='NjExNTI0MmMtYWUwMi00NDk0LWJmMzAtZDA5NzA3ZGVkMDdhYzI0Y2MzNDUtYzYz_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f')
 
-print("--- Starting teams bot ---")
+@app.route("/", methods=['GET'])
+def webhook_events():
+    return message_space("refactor")
 
-@app.route("/")
-def send_message():
+def message_space(txt):
     roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vYzM4ZDE5NTAtYzgwNC0xMWVhLWEyZTgtYjdhOWFhMmIyM2Uy"
-    api.messages.create(roomId, text="Testing123")
+    api.messages.create(roomId, text=txt)
     return "Complete"
-
-@app.route("/message/")
-def get_message():
-    url = "http://webexapis.com/v1/messages?roomId=Y2lzY29zcGFyazovL3VzL1JPT00vYzM4ZDE5NTAtYzgwNC0xMWVhLWEyZTgtYjdhOWFhMmIyM2Uy"
-    headers = {'Authrorization':'Bearer NjExNTI0MmMtYWUwMi00NDk0LWJmMzAtZDA5NzA3ZGVkMDdhYzI0Y2MzNDUtYzYz_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'}
-    try:
-        r = requests.get(url)
-        return r.text
-    except requests.exceptions.ConnectionError:
-        return "error"
-
     
 
+
+
+
+
+
+
+
+
+#forces to listen to all hosts on port 80
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=True)
