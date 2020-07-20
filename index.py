0859adc18f1b0d5decdab26e0e4dcc11954357c0
Simon Light <sipoe900@gmail.com>
2020-07-20 16:13:19 +0100

    SNMP trials (breaks messaging part of code)

--- index.py
+++ index.py
@@ -2,9 +2,43 @@ from flask import Flask
 import requests
 import pysnmp
 from webexteamssdk import WebexTeamsAPI, Webhook
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.entity.rfc3413 import ntfrcv
 
 app = Flask(__name__)
+snmpEngine = engine.SnmpEngine()
 api = WebexTeamsAPI(access_token='NjExNTI0MmMtYWUwMi00NDk0LWJmMzAtZDA5NzA3ZGVkMDdhYzI0Y2MzNDUtYzYz_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f')
+
+TrapAgentAddress='0.0.0.0'; #Trap listerner address
+Port=163;  #trap listerner port
+
+print("Agent is listening SNMP Trap on "+TrapAgentAddress+" , Port : " +str(Port));
+print('--------------------------------------------------------------------------');
+config.addTransport(
+    snmpEngine,
+    udp.domainName + (1,),
+    udp.UdpTransport().openServerMode((TrapAgentAddress, Port))
+)
+
+#Configure community here
+config.addV1System(snmpEngine, 'my-area', 'public')
+
+def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
+          varBinds, cbCtx):
+    print("Received new Trap message")
+    for name, val in varBinds:        
+        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+
+ntfrcv.NotificationReceiver(snmpEngine, cbFun)
+
+snmpEngine.transportDispatcher.jobStarted(1)  
+
+try:
+    snmpEngine.transportDispatcher.runDispatcher()
+except:
+    snmpEngine.transportDispatcher.closeDispatcher()
+    raise
 
 @app.route("/", methods=['GET'])
 def webhook_events():
