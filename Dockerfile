badf802d4795b5c10c329ce1d17313f08fd95f03
Simon Light <sipoe900@gmail.com>
2020-08-04 15:45:18 +0100

    4 aug

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM python:alpine3.7
 RUN pip install --upgrade pip
 COPY . /app
 WORKDIR /app
-RUN apk --update add --virtual build-dependencies libffi-dev openssl-dev python-dev py-pip build-base \
+RUN apk --update add --virtual build-dependencies libffi-dev openssl-dev python-dev py-pip build-base net-snmp \
   && pip install --upgrade pip \
   && pip install -r requirements.txt \
   && apk del build-dependencies
 RUN pip install -r requirements.txt
-EXPOSE 80
+EXPOSE 5000
 CMD python ./index.py
--- index.py
+++ index.py
@@ -1,62 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import pysnmp
 from webexteamssdk import WebexTeamsAPI, Webhook
-from pysnmp.entity import engine, config
-from pysnmp.carrier.asyncore.dgram import udp
-from pysnmp.entity.rfc3413 import ntfrcv
+#from easysnmp import Session
+
 
 app = Flask(__name__)
-snmpEngine = engine.SnmpEngine()
 api = WebexTeamsAPI(access_token='NjExNTI0MmMtYWUwMi00NDk0LWJmMzAtZDA5NzA3ZGVkMDdhYzI0Y2MzNDUtYzYz_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f')
 
-TrapAgentAddress='0.0.0.0'; #Trap listerner address
-Port=163;  #trap listerner port
+@app.route("/", methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'GET':
+        return 'This is the status page'
+    else:
+        data = json.loads(request.data)
+        SNMP_Update(data)
+        return 'Successful Update'
 
-print("Agent is listening SNMP Trap on "+TrapAgentAddress+" , Port : " +str(Port));
-print('--------------------------------------------------------------------------');
-config.addTransport(
-    snmpEngine,
-    udp.domainName + (1,),
-    udp.UdpTransport().openServerMode((TrapAgentAddress, Port))
-)
 
-#Configure community here
-config.addV1System(snmpEngine, 'my-area', 'public')
+def SNMP_Update(data):
+    message_space(data)
+    return True
 
-def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
-          varBinds, cbCtx):
-    print("Received new Trap message")
-    for name, val in varBinds:        
-        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
-
-ntfrcv.NotificationReceiver(snmpEngine, cbFun)
-
-snmpEngine.transportDispatcher.jobStarted(1)  
-
-try:
-    snmpEngine.transportDispatcher.runDispatcher()
-except:
-    snmpEngine.transportDispatcher.closeDispatcher()
-    raise
-
-@app.route("/", methods=['GET'])
-def webhook_events():
-    return message_space("refactor")
 
 def message_space(txt):
     roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vYzM4ZDE5NTAtYzgwNC0xMWVhLWEyZTgtYjdhOWFhMmIyM2Uy"
+    print(txt)
     api.messages.create(roomId, text=txt)
     return "Complete"
-    
-
-
-
-
-
-
-
-
 
 #forces to listen to all hosts on port 80
 if __name__ == "__main__":
